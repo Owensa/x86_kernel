9c4a80bf1c72b307af09a010c378e925b22a276a
ANDREW OWENS <drew.owensg@gmail.com>
2016-10-23 07:47:26 -0400

    added multiboot compliant check procedures, created stack, moving esp per normal

--- src/architecture/boot.asm
+++ src/architecture/boot.asm
@@ -3,6 +3,11 @@ global start
 section .text 
 bits 32
 start:
+	mov esp, stack_top
+	;doing multiboot compliant checks, mb, CPUID, long mode.
+	call check_multiboot
+	call check_cpuid
+	call check_long_mode
 	; print OK to the screen
 	mov dword [0xb8000], 0x2f4b2f4f
 	hlt
@@ -13,3 +18,9 @@ error:
 	mov dword [0xb0008], 0x4f204f20
 	move byte [0xb800a], al
 	hlt
+;init stack memory
+section .bss
+stack_bottom:
+	resb 64
+stack_top:
+
