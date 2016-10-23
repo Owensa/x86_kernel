3c6457970c159663aef9cb2c061687e192e1b7f5
ANDREW OWENS <drew.owensg@gmail.com>
2016-10-23 07:07:22 -0400

    remove binutils, reorganize dir structure, add arch folder

--- binutils-2.27/build/bfd/bfdver.h
+++
@@ -1,4 +0,0 @@
-#define BFD_VERSION_DATE 20160803
-#define BFD_VERSION 227000000
-#define BFD_VERSION_STRING  "(GNU Binutils) " "2.27"
-#define REPORT_BUGS_TO "<http://www.sourceware.org/bugzilla/>"
