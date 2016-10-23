42572d10e71d9201bfc953872465f2f49b9f7484
ANDREW OWENS <drew.owensg@gmail.com>
2016-10-23 07:44:04 -0400

    added error procedure to boot.asm

--- src/architecture/boot.asm
+++ src/architecture/boot.asm
@@ -6,3 +6,10 @@ start:
 	; print OK to the screen
 	mov dword [0xb8000], 0x2f4b2f4f
 	hlt
+;ERR procedure 
+error:
+	mov dword [0xb8000], 0x4f524f45
+	mov dword [0xb8004], 0x4f3a4f52
+	mov dword [0xb0008], 0x4f204f20
+	move byte [0xb800a], al
+	hlt
